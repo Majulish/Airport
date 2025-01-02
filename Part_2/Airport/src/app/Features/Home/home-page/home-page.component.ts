4d22e2d0cc379374c36cdb479d696abb6db4157e
adiam1 <adiamshalem1@gmail.com>
2025-01-02 20:01:55 +0200

    fixing learn more and book now buttons on home-age

--- Part_2/Airport/src/app/Features/Home/home-page/home-page.component.ts
+++ Part_2/Airport/src/app/Features/Home/home-page/home-page.component.ts
@@ -6,12 +6,14 @@ import {Flight} from '../../Flights/Model/filght.module';
 import {MatIconModule} from '@angular/material/icon';
 import {MatCard, MatCardActions, MatCardContent} from '@angular/material/card';
 import {CommonModule} from '@angular/common';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-home-page',
-  imports: [MatIconModule, MatCard, MatCardActions, MatCardContent, CommonModule],
+  imports: [MatIconModule, MatCard, MatCardActions, MatCardContent, CommonModule, RouterLink],
   templateUrl: './home-page.component.html',
-  styleUrl: './home-page.component.css'
+  styleUrl: './home-page.component.css',
+  standalone: true,
 })
 export class HomePageComponent implements OnInit {
   flights!: Flight[];
@@ -26,5 +28,11 @@ export class HomePageComponent implements OnInit {
   ngOnInit() {
     this.flights = this.flightsService.getAllFlightsForNextWeek();
     this.destinations = this.destinationsService.getAllDestinations();
+  }
+
+  openAirportUrl(url: string): void {
+    if (url) {
+      window.open(url, '_blank');
+    }
   }
 }
